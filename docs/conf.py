28f6a59ca144633d213a3e8696633602d861fcba
Garret Tomlin <garrettomlin@Garrets-MacBook-Pro.local>
2025-12-07 07:38:18 -0500

    updates

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+project = 'Opay Integration'
+copyright = '2025, Orbtronics'
+author = 'Orbtronics'
+
+extensions = [
+    'sphinx_rtd_theme',
+    'myst_parser',
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+html_theme = 'sphinx_rtd_theme'
+master_doc = 'index'
